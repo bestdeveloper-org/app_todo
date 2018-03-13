8a856fb0dffc5a7080a48d95e4b42b3dcc7cffed
Mihai Maxim <mihaimaxim90@icloud.com>
2018-03-13 14:22:30 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { CounterComponent } from './ui/counter/counter.component';
 import { CounterWithServiceComponent } from './ui/counter-with-service/counter-with-service.component';
@@ -11,7 +9,6 @@ import { HeaderComponent } from './ui/header/header.component';
 import { FooterComponent } from './ui/footer/footer.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { ValidationExampleComponent } from './ui/validation-example/validation-example.component';
-
 
 @NgModule({
   declarations: [
